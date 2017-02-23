8128866314a324655bd9b7c1eb62c3b9dc656f40
Unknown <avaearthben2@gmail.com>
2017-02-23 00:23:18 -0500

    Added scripts for picking up/dropping objects and moving the player

---
+++ Assets/Script/PickupTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupTrigger : MonoBehaviour
+{
+
+    Collider thingHolding;
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetMouseButton(0) == true)
+        {
+            Debug.Log("Click");
+            other.transform.SetParent(transform);
+            thingHolding = other; 
+            thingHolding.transform.SetParent(transform); 
+        }
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetMouseButton(0) == false && thingHolding != null)
+        {
+            thingHolding.transform.SetParent(null); 
+            thingHolding = null; 
+        }
+    }
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.TransformVector(new Vector3(0f, 0f, .1f));
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.localEulerAngles += new Vector3(0f, -90f * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.localEulerAngles += new Vector3(0f, 90f * Time.deltaTime, 0f);
+        }
+
+    }
+}
