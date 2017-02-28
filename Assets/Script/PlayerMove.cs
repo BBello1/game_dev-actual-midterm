9da9dfc03e0e48ff938887e6c3515fa30d1c29be
Unknown <avaearthben2@gmail.com>
2017-02-27 23:51:27 -0500

    Added more level design

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    float upDownLook = 0f;
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += transform.TransformVector(new Vector3(0f, 0f, .1f));
+            transform.position += transform.TransformVector(new Vector3(0f, 0f, .15f));
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position -= transform.TransformVector(new Vector3(0f, 0f, .1f));
+            transform.position -= transform.TransformVector(new Vector3(0f, 0f, .15f));
         }
 
         if (Input.GetKey(KeyCode.A))
@@ -25,6 +26,17 @@ public class PlayerMove : MonoBehaviour
         {
             transform.localEulerAngles += new Vector3(0f, 90f * Time.deltaTime, 0f);
         }
+
+        
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * 300f; 
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * 200f; 
+        
+        transform.Rotate(0f, mouseX, 0f);
+
+        upDownLook -= mouseY; 
+        upDownLook = Mathf.Clamp(upDownLook, -80f, 80f); 
+
+        transform.localEulerAngles = new Vector3(upDownLook, transform.localEulerAngles.y, 0f);
 
     }
 }
