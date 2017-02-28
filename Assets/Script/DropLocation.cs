7ec5ef3596845e0ea81bc162dd26fc846c182993
Unknown <avaearthben2@gmail.com>
2017-02-28 15:40:53 -0500

    Added mouse camera movement, removed A/D movement, changed layers for floor/suitcase/player trigger

--- Assets/Script/DropLocation.cs
+++ Assets/Script/DropLocation.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class DropLocation : MonoBehaviour
 {
 
-   
+   int items = 0;
 
     void Start()
     {
@@ -19,5 +21,11 @@ public class DropLocation : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
+        items += 1;
+
+        if (items == 3)
+        {
+            SceneManager.LoadScene("End_Scene", LoadSceneMode.Single);
+        }
     }
 }
--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class PlayerMove : MonoBehaviour
 {
     float upDownLook = 0f;
@@ -18,15 +20,6 @@ public class PlayerMove : MonoBehaviour
             transform.position -= transform.TransformVector(new Vector3(0f, 0f, .15f));
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.localEulerAngles += new Vector3(0f, -90f * Time.deltaTime, 0f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.localEulerAngles += new Vector3(0f, 90f * Time.deltaTime, 0f);
-        }
-
         
         float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * 300f; 
         float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * 200f; 
@@ -38,5 +31,11 @@ public class PlayerMove : MonoBehaviour
 
         transform.localEulerAngles = new Vector3(upDownLook, transform.localEulerAngles.y, 0f);
 
-    }
+        
+        if (Input.GetKeyDown(KeyCode.R))
+            {
+            SceneManager.LoadScene("Midterm", LoadSceneMode.Single);
+        }
+
+        }
 }
